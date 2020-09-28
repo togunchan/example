bd20da38bd7a6a55e0123597c7c4c53d44ef7408
meTheLearner <togunchan@gmail.com>
2020-09-29 01:41:56 +0300

    3-Adding Product Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {ProductComponent} from "./product.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    ProductComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ProductComponent] // başlangıç "component"i
 })
 export class AppModule { }
