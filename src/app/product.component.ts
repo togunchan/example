58d1e28a9bc2103ad7010de760b7b6ccc8da3b4d
meTheLearner <togunchan@gmail.com>
2020-10-01 21:49:59 +0300

    5-Class Binding

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -11,8 +11,22 @@ export class ProductComponent{
 
     //yeni bir "property" tanımlanıp ona bir nesne atandı.
     model: ProductRepository = new ProductRepository();
+    disabled = true;
 
-    //atanan nesne üzerinden metota ulaşabildik
-    product: Product = this.model.getProductById(1);
+    getClasses(id: number): string{
+        let product = this.model.getProductById(id);
+        return (product.price <=1000 ? "bg-info" : "bg-secondary")+" m-2 p-2 text-white";
+    }
+
+    getClassMap(id: number): Object{
+        let product = this.model.getProductById(id);
+        return {
+            //":"dan sonraki değerler "true" ise değer döndürülür.
+            "bg-info": product.price <= 1000,
+            "bg-secondary": product.price >1000,
+            "text-center text-white": product.name == "Samsung s6"
+        }
+    }
+
 
 }
