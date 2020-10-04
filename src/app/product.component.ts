88357a1d64f59cf5e5ade66ba864aa39f852c924
meTheLearner <togunchan@gmail.com>
2020-10-04 19:09:08 +0300

    9-Pipes

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -8,23 +8,40 @@ import { ProductRepository } from './repository.model';
     selector: "app",
     /*<input [value]="email" type="text" (keyup.enter)="email= $event.target.value; onKeyUp()">*/
     template: `
-    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()">
-    <br>
-    <span>
-        {{email}}
-    </span>
+    <h4> Lower-Upper-Title CasePipe </h4>
+    <p> {{title}} </p>
+    <p> {{title | lowercase}} </p>
+    <p> {{title | uppercase}} </p>
+    <p> {{title | titlecase}} </p>
+
+    <h4> Date Pipe </h4>
+    <p> {{today}} </p>
+    <p> {{today | date}} </p>
+    <p> {{today | date: 'fullDate'}} </p>
+    <p> {{today | date: 'medium'}} </p>
+    <p> {{today | date: 'shortTime'}} </p>
+    <p> {{today | date: 'h:mm:ss'}} </p>
+
+    <h4> Decimal Pipe </h4>
+    <p> {{students}} </p>
+    <p> {{students | number}} </p>
+    <p> {{price | number: '1.2-2'}} </p>
+    
+    <h4> Currency Pipe </h4>
+    <p> {{price | currency: 'EUR'}} </p>
+    
+    <h4> Percent Pipe </h4>
+    <p> {{completed | percent }} </p>
+    <p> {{completed | percent: '2.2-2'}} </p>
     `,
     styleUrls: ["product.component.css"]
 })
 export class ProductComponent {
 
-    //yeni bir "property" tanımlanıp ona bir nesne atandı.
-    model: ProductRepository = new ProductRepository();
-
-    email= "email@gmail.com";
-
-    onKeyUp(){
-            console.log(this.email);
-    }
+    today: number = Date.now();
+    title: string = 'Angular Kursu';
+    students: number = 21536;
+    price: number = 395.9768;
+    completed: number = 0.26;
 
 }
