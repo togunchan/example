2cadf483d2b9d36413f82c30a0f2e238fee34cbc
meTheLearner <togunchan@gmail.com>
2020-10-04 18:29:34 +0300

    8-KeyUp Event

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -1,52 +1,30 @@
 import { Component } from "@angular/core";
+import { FormsModule } from '@angular/forms';
 import { Product } from './product.model';
 import { ProductRepository } from './repository.model';
 
+
 @Component({
     selector: "app",
-    templateUrl: "product.component.html",
+    /*<input [value]="email" type="text" (keyup.enter)="email= $event.target.value; onKeyUp()">*/
+    template: `
+    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()">
+    <br>
+    <span>
+        {{email}}
+    </span>
+    `,
     styleUrls: ["product.component.css"]
 })
 export class ProductComponent {
 
     //yeni bir "property" tanımlanıp ona bir nesne atandı.
     model: ProductRepository = new ProductRepository();
-    disabled = true;
 
-    getClasses(id: number): string {
-        let product = this.model.getProductById(id);
-        return (product.price <= 1000 ? "bg-info" : "bg-secondary") + " m-2 p-2 text-white";
-    }
+    email= "email@gmail.com";
 
-    getClassMap(id: number): Object {
-        let product = this.model.getProductById(id);
-        return {
-            //":"dan sonraki değerler "true" ise değer döndürülür.
-            "bg-info": product.price <= 1000,
-            "bg-secondary": product.price > 1000,
-            "text-center text-white": product.name == "Samsung s6"
-        }
-    }
-
-    color: string = this.model.getProductById(1).price <= 1000 ? "green" : "red";
-    fontSize: string = "25px";
-
-    getStyles(id: number) {
-        let product = this.model.getProductById(1);
-        return {
-            fontSize: "25px",
-            color: this.model.getProductById(1).price <= 1000 ? "green" : "red"
-        }
-    }
-
-    onSubmit($event){
-        $event.stopPropagation();
-        console.log("button was clicked")
-        console.log($event);
-    }
-
-    onDivClicked(){
-        console.log("div was clicked")
+    onKeyUp(){
+            console.log(this.email);
     }
 
 }
